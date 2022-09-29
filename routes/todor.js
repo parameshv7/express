22d8be2a78c075ca54701bc1165029ceea5691cf
Parameshwaran Venkatesan <parameshvenkat98@gmail.com>
2022-09-29 19:44:02 +0530

    asdg

--- routes/todor.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { createTodo, getTodolist } = require("../controllers/todo.js");
-const router = express.Router();
-
-router.get("/todos", getTodolist);
-router.post("/todo", createTodo);
-
-module.exports = router;
