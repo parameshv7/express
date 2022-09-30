47efe5f657de4e700317610e172f48c3912df5c1
Parameshwaran Venkatesan <parameshvenkat98@gmail.com>
2022-09-30 10:09:24 +0530

    ft

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 
 
-import bodyParser from "body-parser";
-import cors from "cors";
-import todo from "./routes/todor.js"
+// //import bodyParser from "body-parser";
+// //import cors from "cors";
+// //import todo from "./routes/todor.js"
+// const express = require("express");
+
+
+// const app = express();
+// //app.use(bodyParser.json());
+// //app.use(cors());
+// //app.use("/", todo);
+
+
+
+// app.get("/", (req, res) => {
+//   res.send("sscxxxwwwww on Vercel");
+// });
+
+// app.listen(9000, () => {
+//   console.log("Running on port 9000.");
+// });
+
+// // Export the Express API
+// export default app;
+
+
 const express = require("express");
 
-
 const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-app.use("/", todo);
-
-
 
 app.get("/", (req, res) => {
-  res.send("sscxxxwwwww on Vercel");
+  res.send("Express on Vercel");
 });
 
-app.listen(9000, () => {
-  console.log("Running on port 9000.");
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
 });
 
 // Export the Express API
-export default app;
+module.exports = app;
