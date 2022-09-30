b6e6650513a5dbdba62005fb28dec6689a177873
Parameshwaran Venkatesan <parameshvenkat98@gmail.com>
2022-09-30 11:06:24 +0530

    fr

--- index.js
+++ index.js
@@ -28,13 +28,11 @@
 //const express = require("express");
 
 
-import bodyParser from "body-parser";
-import cors from "cors";
+
 import express from "express";
 
 const app = express();
-app.use(bodyParser.json());
-app.use(cors());
+
 
 app.get("/", (req, res) => {
   res.send("PS1");
